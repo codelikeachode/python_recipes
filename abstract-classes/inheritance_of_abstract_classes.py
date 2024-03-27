7906e409bc7a2ecc92685e6070d54a4ad0dfebe9
codelikeachode <90079899+codelikeachode@users.noreply.github.com>
2024-03-27 17:43:40 -0400

    added recip[e

---
+++ abstract-classes/inheritance_of_abstract_classes.py
@@ -0,0 +1,28 @@
+from abc import *
+
+class AVehicle(ABC):
+    @property
+    @abstractmethod
+    def max_speed(self):
+        pass
+
+class ATruck(AVehicle):
+    @property
+    @abstractmethod
+    def capacity(self):
+        pass
+
+class Kamaz5320(ATruck):
+    @property
+    def max_speed(self):
+        return 85
+    
+    @property
+    def capacity(self):
+        return 8000
+    
+kamaz = Kamaz5320()
+maxSpeed = kamaz.max_speed
+# maxSpeed = 85
+
+print(maxSpeed)
