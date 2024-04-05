d145f0e717f79f320a88b99aaad04380a0ac3723
codelikeachode <90079899+codelikeachode@users.noreply.github.com>
2024-04-05 15:58:38 -0400

    added recipe

---
+++ simple-types/boolean/converting_to_string.py
@@ -0,0 +1,10 @@
+sunIsStar = True
+strSun = str(sunIsStar)
+# strStar is "True"
+
+earthIsStar = False
+strEarth = "%s" % earthIsStar
+# strEarth is "False"
+
+print(f"{strSun = }")
+print(f"{strEarth = }")
