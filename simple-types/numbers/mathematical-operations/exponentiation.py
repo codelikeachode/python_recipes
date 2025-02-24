e0af4745e5b35f426a6cf50150b76277c6e6ecde
codelikeachode <90079899+codelikeachode@users.noreply.github.com>
2025-02-24 13:01:18 -0500

    more

---
+++ simple-types/numbers/mathematical-operations/exponentiation.py
@@ -0,0 +1,12 @@
+number = 8
+power = 3
+result1 = pow(number, power)
+
+result2 = number ** power
+
+result3 = pow(number, power, 100)
+
+print(f"{result1 = }")
+print(f"{result2 = }")
+print(f"{result3 = }")
+print(8 ** 3 % 100)
